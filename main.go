e9107ce911fcac638f9cf88bcd173b05930949a3
fullgukbap <2hanjunbum6@gmail.com>
2024-09-10 12:30:42 +0900

    caching middlware 추가

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/helmet"
 	"github.com/gofiber/fiber/v2/middleware/recover"
@@ -32,27 +35,36 @@ func init() {
 func main() {
 
 	// Fiber 앱 생성
-	app := fiber.New(fiber.Config{})
+	app := fiber.New(fiber.Config{
+		GETOnly: true,
+	})
 
 	app.Use(recover.New())
 
 	// 압축 미들웨어 추가
 	app.Use(compress.New(compress.Config{
 		Level: compress.LevelBestSpeed, // 압축 속도를 최적화하려면 BestSpeed 사용
-
 	}))
 
 	app.Use(helmet.New())
 
-	app.All("/*", func(c *fiber.Ctx) error {
+	app.Use(cache.New(cache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Query("noCache") == "true"
+		},
+		Expiration:   12 * time.Hour, // 12 h
+		CacheControl: true,
+	}))
 
+	// 만약 나중에 요청이 많이 올 시  아래 미들웨어 추가
+	// Etga, Limiter
+
+	app.All("/*", func(c *fiber.Ctx) error {
+		fmt.Println("hi")
 		key := c.Query("key", "")
 		if key == "" || key != config.Auth.Key {
 			return c.SendStatus(fiber.StatusUnauthorized)
 		}
-
-		// 캐시 헤더 설정
-		c.Set("Cache-Control", "public, max-age=86400") // 1일 동안 캐싱
 
 		c.Set("Content-Type", "application/pdf")
 		c.Set("Content-Disposition", "inline; filename=한준범 포토폴리오.pdf")
